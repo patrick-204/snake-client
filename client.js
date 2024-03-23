42125e6d1d89d12c1f8ea4b3fa49b440cc4ac3b3
patrick-204 <p.ohara921@gmail.com>
2024-03-22 21:59:48 -0400

    Corrected mistake. Added stdin to the play module instead of the client module. Called the setupinput function at end of play.

--- client.js
+++ client.js
@@ -31,25 +31,4 @@ const connect = function() {
   return conn;
 };
 
-// specifies what happens when data is received from stdin
-// (when key is pressed on keyboard)
-const handleUserInput = function(key) {
-  // if ctrl+c is pressed then exit game
-  if (key === "\u0003") {
-    process.exit();
-  }
-}
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-handleUserInput();
-
 module.exports = connect;
--- play.js
+++ play.js
@@ -2,3 +2,24 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// specifies what happens when data is received from stdin
+// (when key is pressed on keyboard)
+const handleUserInput = function(key) {
+  // if ctrl+c is pressed then exit game
+  if (key === "\u0003") {
+    process.exit();
+  }
+}
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
